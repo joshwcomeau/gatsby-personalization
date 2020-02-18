ba2db54314d42352e3e73fbbc1f6b3d09c1ae218
Josh Comeau <joshwcomeau@gmail.com>
2020-02-18 18:20:36 -0500

    Fixed

--- src/components/client-only.js
+++ src/components/client-only.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import './client-only.css';
+
+const isBeingPrebuilt =
+  typeof window === 'undefined';
+
 const ClientOnly = ({ children }) => {
+  if (isBeingPrebuilt) {
+    return null;
+  }
+
   return (
     <div className="client-only">{children}</div>
   );
